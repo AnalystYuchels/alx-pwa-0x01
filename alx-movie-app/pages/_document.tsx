21aabab9a46cf61e3d3f83ea2d0845ec3b6a351a
AnalystYuchels <uche4xploit@gmail.com>
2025-08-22 02:51:16 +0100

    Linked the manifest in document.js

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,8 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme color (matches manifest) */}
+        <meta name="theme-color" content="#FFFFFF" />
+
+        {/* Icons */}
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-chrome-192x192.png" />
+
+        {/* Microsoft tile (optional but useful since you have the asset) */}
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-TileImage" content="/icons/ms-icon-310x310.png" />
+
+        {/* Let iOS run as a standalone web app */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
